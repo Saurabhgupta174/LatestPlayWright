d3cc6ac5be729bcef73a6cbc7e1930d45de3ec6e
Saurabh Gupta <sgupta174@gmail.com>
2026-05-09 19:25:58 +0530

    Changes in login file
    :wq
    
    wq

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -7,3 +7,13 @@ test("should login successfully", async ({ page }) =>
     await page.pause();
     
 });
+
+test("Login Funactinality", async ({ page }) => 
+{
+    await page.goto("https://www.saucedemo.com/");
+    await page.locator("##user-name").fill("standard_user");
+    await page.locator("#password").fill("secret_sauce");
+    await page.locator("#login-button").click();
+    await page.pause();
+    
+});
