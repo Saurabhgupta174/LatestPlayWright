8d853d0c11fb7971c2ffb88a72611c9107da5b16
Saurabh Gupta <sgupta174@gmail.com>
2026-05-17 19:49:05 +0530

    Add some new pages

--- playwright.config.js
+++ playwright.config.js
@@ -18,7 +18,12 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter:[ ['html', { open: 'on-failure' }] ],
-  timeout: 30000,
+  timeout: 300000,
+  
+  expect: {
+    timeout: 300000
+  },
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     trace: 'on-first-retry',
